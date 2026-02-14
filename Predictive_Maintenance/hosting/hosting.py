66e3e2d82483c4a0af845a22936fb75c0c7aeb19
vinodcwanted <vinodcwanted@gmail.com>
2026-02-14 16:54:18 +0530

    Initial commit with correct nested structure

--- Predictive_Maintenance/hosting/hosting.py
+++ Predictive_Maintenance/hosting/hosting.py
@@ -4,7 +4,7 @@ import os
 api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
     folder_path="Predictive_Maintenance/deployment",     # the local folder containing your files
-    repo_id="vinodcwanted/Predictive_Maintenance",          # the target repo
+    repo_id="vinodcwanted/Predictive-Maintenance",          # the target repo
     repo_type="space",                      # dataset, model, or space
     path_in_repo="",                          # optional: subfolder path inside the repo
 )
